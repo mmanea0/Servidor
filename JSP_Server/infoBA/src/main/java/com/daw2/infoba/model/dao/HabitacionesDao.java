8acc94990eeb4ade7c0a3cb45d88a467632f6eea
Luis Miguel <luismguapo@iescastelar.com>
2022-09-22 20:03:36 +0200

    habitacion3.0

--- JSP_Server/infoBA/src/main/java/com/daw2/infoba/model/dao/HabitacionesDao.java
+++ JSP_Server/infoBA/src/main/java/com/daw2/infoba/model/dao/HabitacionesDao.java
@@ -3,6 +3,8 @@ package com.daw2.infoba.model.dao;
 import com.daw2.infoba.model.entity.Habitacion;
 import com.daw2.infoba.model.entity.Proveedor;
 
-public interface HabitacionesDao extends Dao<Habitacion, Integer> {
+import java.util.List;
 
+public interface HabitacionesDao extends Dao<Habitacion, Integer> {
+    List <Habitacion> listAllFillHabitacion();
 }
--- JSP_Server/infoBA/src/main/java/com/daw2/infoba/model/dao/impl/HabitacionesDaoImpl.java
+++ JSP_Server/infoBA/src/main/java/com/daw2/infoba/model/dao/impl/HabitacionesDaoImpl.java
@@ -1,13 +1,30 @@
 package com.daw2.infoba.model.dao.impl;
+import com.daw2.infoba.model.dao.ConectaBD;
 import com.daw2.infoba.model.dao.HabitacionesDao;
 import com.daw2.infoba.model.entity.Habitacion;
+import com.daw2.infoba.model.entity.Proveedor;
 
-import java.sql.Date;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class HabitacionesDaoImpl implements HabitacionesDao {
 
+    private Connection  connection;
+
+    private boolean error;
+
+    public HabitacionesDaoImpl(){
+        super();
+        error=false;
+        try {
+           connection = ConectaBD.getConnection();
+        }catch (SQLException e){
+            error = true;
+            System.out.println("Se ha producido un erro ¡r al acceder a la BD");
+        }
+    }
 
     @Override
     public Integer add(Habitacion entity) {
@@ -62,5 +79,10 @@ public class HabitacionesDaoImpl implements HabitacionesDao {
     @Override
     public java.util.Date sqlDateToUtilDate(Date date) {
         return HabitacionesDao.super.sqlDateToUtilDate(date);
+    }
+
+    @Override
+    public List<Habitacion> listAllFillHabitacion() {
+        return null;
     }
 }
