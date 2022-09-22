b4d174cd72c6fc75d94cbd9d43867a1fcbca283d
Luis Miguel <luismguapo@iescastelar.com>
2022-09-22 20:23:09 +0200

    habitacion3.0

--- JSP_Server/infoBA/src/main/java/com/daw2/infoba/model/dao/impl/HabitacionesDaoImpl.java
+++ JSP_Server/infoBA/src/main/java/com/daw2/infoba/model/dao/impl/HabitacionesDaoImpl.java
@@ -1,6 +1,8 @@
 package com.daw2.infoba.model.dao.impl;
 import com.daw2.infoba.model.dao.ConectaBD;
 import com.daw2.infoba.model.dao.HabitacionesDao;
+import com.daw2.infoba.model.dao.ProveedoresDao;
+import com.daw2.infoba.model.entity.Articulo;
 import com.daw2.infoba.model.entity.Habitacion;
 import com.daw2.infoba.model.entity.Proveedor;
 
@@ -11,16 +13,16 @@ import java.util.List;
 
 public class HabitacionesDaoImpl implements HabitacionesDao {
 
-    private Connection  connection;
+    private Connection connection;
 
     private boolean error;
 
-    public HabitacionesDaoImpl(){
+    public HabitacionesDaoImpl() {
         super();
-        error=false;
+        error = false;
         try {
-           connection = ConectaBD.getConnection();
-        }catch (SQLException e){
+            connection = ConectaBD.getConnection();
+        } catch (SQLException e) {
             error = true;
             System.out.println("Se ha producido un erro ¡r al acceder a la BD");
         }
@@ -28,61 +30,85 @@ public class HabitacionesDaoImpl implements HabitacionesDao {
 
     @Override
     public Integer add(Habitacion entity) {
-        return null;
+        Integer id = null;
+        error = false;
+        try {
+            id = _add(entity);
+            connection.commit();
+        } catch (SQLException ex) {
+            error = true;
+            try {
+                connection.rollback();
+            } catch (SQLException ex1) {
+            }
+        }
+        return id;
     }
 
-    @Override
-    public boolean add(List<Habitacion> list) {
-        return false;
-    }
-
-    @Override
-    public boolean update(Habitacion entity) {
-        return false;
-    }
-
-    @Override
-    public boolean delete(int id) {
-        return false;
-    }
-
-    @Override
-    public boolean deleteAll() {
-        return false;
-    }
-
-    @Override
-    public Habitacion get(int id) {
-        return null;
-    }
-
-    @Override
     public List<Habitacion> listAll() {
-        return null;
+        error = false;
+        Habitacion entity = null;
+        List<Habitacion> list = new ArrayList<>();
+        String sql = "SELECT "
+                + "id, numero, capacidad, fumador, descripcion, created_at, modified_at "
+                + "FROM habitaciones";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                entity = new Habitacion();
+                entity.setId(rs.getInt("id"));
+                entity.setNumero(rs.getString("numero"));
+                entity.setCapacidad(rs.getInt("capacidad"));
+                entity.setFumador(rs.getBoolean("fumador"));
+                Habitacion habitacion = new Habitacion();
+                habitacion.setId(rs.getInt("habitacion_id"));
+                entity.setHabitacion(habitacion);
+                entity.setCreatedAt(rs.getDate("created_at"));
+                entity.setModifiedAt(rs.getDate("modified_at"));
+                list.add(entity);
+            }
+            rs.close();
+        } catch (SQLException ex) {
+            error = true;
+            list = null;
+        }
+        return list;
     }
 
     @Override
     public boolean isError() {
-        return false;
+        return error;
     }
 
-    @Override
-    public boolean exist(int id) {
-        return HabitacionesDao.super.exist(id);
+    private Integer _add(Habitacion entity) throws SQLException {
+        Integer id;
+        String sql = "INSERT INTO habitaciones "
+                + "(numero, capacidad, fumador, descripcion, created_at, modified_at) VALUES "
+                + "(?,?,?,?,?,?)";
+        PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, entity.getNumero());
+        ps.setString(2, entity.getCapacidad());
+        ps.setDouble(3, entity.getFumador());
+        ps.setInt(4, entity.getHabitacion!=null?entity.getHabitacion().getId():null);
+        // ps.setDate(7, new java.sql.Date(entity.getFechaAlta().getTime()));
+        ps.setDate(5, utilDateToSqlDate(entity.getCreatedAt()));
+        ps.setDate(6, utilDateToSqlDate(entity.getModifiedAt()));
+        ps.executeUpdate();
+        ResultSet rs = ps.getGeneratedKeys();
+        if (rs.next()) {
+            id = rs.getInt(1);
+        } else {
+            id = -1; // Si se ha añadido pero no se ha devuelto un id
+        }
+
+        return id;
     }
 
-    @Override
-    public Date utilDateToSqlDate(java.util.Date date) {
-        return HabitacionesDao.super.utilDateToSqlDate(date);
-    }
 
-    @Override
-    public java.util.Date sqlDateToUtilDate(Date date) {
-        return HabitacionesDao.super.sqlDateToUtilDate(date);
-    }
-
-    @Override
-    public List<Habitacion> listAllFillHabitacion() {
-        return null;
+    public List<Habitacion> listAllFillProveedor() {
+        HabitacionesDao habitacionesDao = new HabitacionesDaoImpl();
+        List<Habitacion> list = listAll();
+        list.forEach(e->e.setHabitacion(habitacionesDao.get(e.getHabitacion().getId())));
+        return list;
     }
 }
